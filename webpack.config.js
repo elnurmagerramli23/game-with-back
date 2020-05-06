788259cbc431cfa8361cc79db819898615711d96
fara0000 <fexri_imanzade@mail.ru>
2020-05-07 02:36:40 +0400

    now, you can use webpack for back

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,14 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CssWebpackPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    taget: 'node',
     entry: path.resolve(__dirname, "./src/index.js"),
     output: {
         path: path.resolve(__dirname, "./build"),
         filename: "index.js"
     },
-    devServer: {
-        port: 3000,
-        contentBase: path.resolve(__dirname, "./build"),
-        hot: true,
-        open: true,
-        watchContentBase: true,
-        watchOptions: {
-            ignored: /node_modules/
-        },
-        writeToDisk: filePath => {
-            return /(index.html$)|(index.js$)|(style.css$)/.test(filePath)
-        }
-    },
-    optimization: {
-        minimize: false,
-    },
     module: {
         rules: [
-            {test: /\.html$/, use: "html-loader"},
-            {test: /\.less$/, use: [CssWebpackPlugin.loader, "css-loader", "less-loader"]},
             {test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
@@ -37,28 +18,10 @@ module.exports = {
                         plugins: ['@babel/plugin-proposal-class-properties']
                     }
                 }
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].[ext]",
-                            outputPath: './images',
-                        },
-                    }
-                ]
             }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "./src/index.html"),
-            filename: "index.html"
-        }),
-        new CssWebpackPlugin({
-            filename: "style.css"
-        })
-    ]
+    node: {
+        __dirname: false,
+    }
 }
