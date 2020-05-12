f571e62ca78b1378ff43e811a33ff9a841e8870b
SevincMammadova <sevincmammadova609@gmail.com>
2020-05-12 09:54:12 +0400

    requast's done

--- src/App.js
+++ src/App.js
@@ -13,16 +13,14 @@ class App{
     
     getApp = () => this.app;
 
-    // GET '/getItems' просьба от front-end
-    // это запрос который ждет массив от сервера
+    
     onGet = (request,response) =>  {
         const data = this.controller.getItems()
         response.json(data);
         response.end();
     }
     
-    // PUT '/setItems' просьба от front-end
-    // запрос на то чтобы поставить массив на сервер при каждом шаге игры
+  
     onPut = (request, response) => {
         const {body} = request;
         this.controller.setItems(JSON.parse(body));
